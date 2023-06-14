bb34b1b1b4b64879ed0243d678eb6fc13dbdabff
miahro <mikko.ahro@gmail.com>
2023-06-14 14:36:36 +0300

    Ex 8.23

--- library-backend/src/index.js
+++ library-backend/src/index.js
@@ -8,16 +8,12 @@ const express = require("express");
 const cors = require("cors");
 const http = require("http");
 
-//const { startStandaloneServer } = require("@apollo/server/standalone");
 const jwt = require("jsonwebtoken");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
-
-//require("dotenv").config({ path: "../.env" });
-//require("dotenv");
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
@@ -28,11 +24,7 @@ const User = require("./models/user");
 const { WebSocketServer } = require("ws");
 const { useServer } = require("graphql-ws/lib/use/ws");
 
-//MONGODB_URI = `mongodb+srv://testuser:666testvitunuser@cluster0.lngrmwx.mongodb.net/?retryWrites=true&w=majority`;
 const url = process.env.MONGODB_URI;
-//const testpath = process.env.MONGODB_URI;
-//console.log("test path", testpath);
-//const url = MONGODB_URI;
 console.log("connecting to MongoDB");
 mongoose
   .connect(url)
@@ -100,25 +92,3 @@ const start = async () => {
 };
 
 start();
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
-
-// startStandaloneServer(server, {
-//   listen: { port: 4000 },
-//   context: async ({ req, res }) => {
-//     const auth = req ? req.headers.authorization : null;
-//     if (auth && auth.toLowerCase().startsWith("bearer ")) {
-//       const decodedToken = jwt.verify(
-//         auth.substring(7),
-//         process.env.JWT_SECRET
-//       );
-//       const currentUser = await User.findById(decodedToken.id);
-//       return { currentUser };
-//     }
-//   },
-// }).then(({ url }) => {
-//   console.log(`Server ready at ${url}`);
-// });
